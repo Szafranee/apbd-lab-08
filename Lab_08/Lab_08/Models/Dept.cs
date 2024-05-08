04fa6bdaaca4fac8fb0c2252160609a7d63663ac
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-05-08 12:53:18 +0200

    init

---
+++ Lab_08/Lab_08/Models/Dept.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab_08.Models
+{
+    public class Dept
+    {
+        public int Deptno { get; set; }
+        public string Dname { get; set; } = string.Empty;
+        public string Loc { get; set; } = string.Empty;
+    }
+}
